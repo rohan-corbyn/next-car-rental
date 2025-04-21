14fc204b8b7b86233575ddc3ba90bd64bf101b62
rohancorbyn <17966183+rohan-corbyn@users.noreply.github.com>
2025-04-21 20:16:13 +0100

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-import Navbar from './ui/navbar';
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import Navbar from "./ui/navbar";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen bg-yellow-100 antialiased`}>
+      <body
+        className={`${inter.className} min-h-screen bg-yellow-100 antialiased`}
+      >
         <Navbar />
-        <div className={`${inter.className} flex flex-col p-6`}>{children}</div>
+        <div className={`${inter.className} mt-20 flex flex-col pt-6`}>
+          {children}
+        </div>
       </body>
     </html>
   );
